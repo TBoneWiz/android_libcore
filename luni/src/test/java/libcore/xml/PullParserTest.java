TBoneWiz/android_libcore

commit 0424d12f9b8a19c4a451282bc8ae974f6109424d

Get stricter about DOCTYPEs.
Allow them only before we've started parsing any
XML elements.

bug: 3452274
Change-Id: I7b146e4dae8c7aa23a0a227538ec02604f828400

xml/src/main/java/org/kxml2/io/KXmlParser.java
@@ -163,6 +163,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
     private boolean degenerated;
     private int attributeCount;
 
+    // true iff. we've encountered the START_TAG of an XML element at depth == 0;
+    private boolean parsedTopLevelStartTag;
+
     /*
      * The current element's attributes arranged in groups of 4:
      * i + 0 = attribute namespace URI
@@ -416,6 +419,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
                 break;
             case DOCDECL:
                 readDoctype(justOneToken);
+                if (parsedTopLevelStartTag) {
+                    throw new XmlPullParserException("Unexpected token", this, null);
+                }
                 break;
 
             default:
@@ -1128,6 +1134,9 @@ public class KXmlParser implements XmlPullParser, Closeable {
         }
 
         int sp = depth++ * 4;
+        if (depth == 1) {
+            parsedTopLevelStartTag = true;
+        }
         elementStack = ensureCapacity(elementStack, sp + 4);
         elementStack[sp + 3] = name;
 

commit c0cd879dd4853a1156a4f6cbd1c994481a30bdce

Squashed commit from LineageOS
Handle whitespaces after DOCTYPE element

This change fixes a bug where a whitespace after DOCTYPE element
in a file would cause the file to be unparsable. It also adds
a regresion test.

bug: https://code.google.com/p/android/issues/detail?id=171828

Wojciech Staszkiewicz committed on May 6, 2015

From https://github.com/LineageOS/android_libcore/commit/d343b157ab7b919467f1bdb06b6c5c7b49437049#diff-38c00f369760f1379d68a9d494a2f698

 Fix PullParser reuse regression

Commit 0424d12 introduced
a regression: calling setInput() would not fully reset the
parser iff the parser had already parsed a <!DOCTYPE
tag.

This change contains a fix and a test.

Thanks to mike@ for the report.

Bug: https://code.google.com/p/android/issues/detail?id=182605

From https://github.com/LineageOS/android_libcore/commit/8cfc47957f397cc0066cd923711671533f72374b#diff-38c00f369760f1379d68a9d494a2f698

xml/src/main/java/org/kxml2/io/KXmlParser.java
@@ -612,6 +612,7 @@ public class KXmlParser implements XmlPullParser, Closeable {
         }
 
         read('>');
+        skip();
     }
 
     /**
@@ -1663,6 +1664,7 @@ public class KXmlParser implements XmlPullParser, Closeable {
         this.reader = reader;
 
         type = START_DOCUMENT;
+        parsedTopLevelStartTag = false;
         name = null;
         namespace = null;
         degenerated = false;

luni/src/test/java/libcore/xml/PullParserTest.java
@@ -766,6 +766,30 @@ public abstract class PullParserTest extends TestCase {
         assertRelaxedParseFailure("<!DOCTYPE foo [<!ELEMENT foo EMPTY"); // EOF in read('>')
     }
 
+    public void testWhitespacesAfterDOCTYPE() throws Exception {
+        XmlPullParser parser = newPullParser();
+        String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE root [\n" +
+                "<!ENTITY dummy \"dummy\">\n" +
+                "]>  \n" +
+                "<root></root>";
+        assertParseSuccess(test, parser);
+    }
+
+    // Regression test for https://code.google.com/p/android/issues/detail?id=182605
+    public void testSetInputParserReuse() throws Exception {
+        XmlPullParser parser = newPullParser();
+        String test = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE root [\n" +
+                "<!ENTITY dummy \"dummy\">\n" +
+                "]>  \n" +
+                "<root></root>";
+        assertParseSuccess(test, parser);
+
+        // A second call to parser.setInput() on a parser should result in a fully-reset parser.
+        assertParseSuccess(test, parser);
+    }
+
     private void assertParseFailure(String xml) throws Exception {
         XmlPullParser parser = newPullParser();
         assertParseFailure(xml, parser);
@@ -784,6 +808,12 @@ public abstract class PullParserTest extends TestCase {
             }
             fail();
         } catch (XmlPullParserException expected) {
+        }
+    }
+
+    private void assertParseSuccess(String xml, XmlPullParser parser) throws Exception {
+        parser.setInput(new StringReader(xml));
+        while (parser.next() != XmlPullParser.END_DOCUMENT) {
         }
     }
 